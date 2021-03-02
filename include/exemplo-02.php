505f3580d198618914eb9e9e5cf99f1839eec9e6
Antonio <antoniocarlosjr850@gmail.com>
2021-03-02 08:19:39 -0300

    Pasta com arquivos contendo funções e como chama-las

---
+++ include/exemplo-02.php
@@ -0,0 +1,12 @@
+<?php
+
+    //include "exemplo-01.php";
+    //require "exemplo-01.php";
+    //include_once "exemplo-01.php";
+    require_once "exemplo-01.php"; //é recomendado usar o require_once
+
+
+    $resultado = somar(5, 10);
+    echo $resultado;
+
+?>
